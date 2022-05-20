7259ccd49bda01a507391950ff419d015660f082
Trivadan <danstanochieng@gmail.com>
2022-05-20 18:17:52 +0300

    menu added

--- js/main.js
+++ js/main.js
@@ -1,57 +1,66 @@
 $(document).ready(function() {
   "use strict";
 
-  /*----------------------------------
-     Quantity Plus Minus
-     ------------------------------------*/
-    function qnt_incre() {
-        $(".qtyBtn").on("click", function () {
-            var qtyField = $(this).parent(".qtyField"),
-                    oldValue = $(qtyField).find(".qty").val(),
-                    newVal = 1;
-
-            if ($(this).is(".plus")) {
-                newVal = parseInt(oldValue) + 1;
-            } else if (oldValue > 1) {
-                newVal = parseInt(oldValue) - 1;
-            }
-            $(qtyField).find(".qty").val(newVal);
-        });
+    if ($.fn.classyNav) {
+        $('#roamingnav').classyNav();
     }
-    qnt_incre();
-   
-});
 
-// Steps Slider
-$('#steps').owlCarousel({
-    loop: false,
-    nav: false,
-    dots: false,
-    autoplayHoverPause: true,
-    autoplay: false,
-    margin: 30,
-    responsive: {
-        0: {
-            items: 1,
-            nav: false,
-            dots: true,
-            autoplay: true,
-            loop: true,
-        },
-        576: {
-            items: 2,
-        },
-        768: {
-            items: 2,
-        },
-        1200: {
-            items: 3,
+    // scrollspy
+    $('body').scrollspy({
+        offset: 190,
+        target: '.dtr-scrollspy'
+    });
+    
+    // nav scroll   
+    var myoffset = $('#dtr-header-global').height();
+    $('.navbar a[href^="#"]').click(function(){  
+        event.preventDefault();  
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - myoffset
+        }, "slow","easeInSine");
+    });
+
+    //stickyatTop
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 670) {
+            $(".scrollheader").addClass("is-sticky");
+            $('.scrollheader').css('position', 'fixed');
+        } else {
+            $(".scrollheader").removeClass("is-sticky");
+            $(".scrollheader").css('position', 'relative');
         }
-    }
-});
+    });
 
-// Popular
-$('#popular').owlCarousel({
+    // Steps Slider
+    $('#steps').owlCarousel({
+        loop: false,
+        nav: false,
+        dots: false,
+        autoplayHoverPause: true,
+        autoplay: false,
+        margin: 30,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                dots: true,
+                autoplay: true,
+                loop: true,
+            },
+            576: {
+                items: 2,
+            },
+            768: {
+                items: 2,
+            },
+            1200: {
+                items: 3,
+            }
+        }
+    });
+
+    // Popular
+    $('#popular').owlCarousel({
     loop: false,
     nav: true,
     navText: ['<img src="./images/icons/left.png">','<img src="./images/icons/right.png">'],
@@ -78,3 +87,5 @@ $('#popular').owlCarousel({
         }
     }
 });
+
+    });
