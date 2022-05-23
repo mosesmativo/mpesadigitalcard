413cdf0bcf11ae73a3a664ceaf0bbaa669cabd7e
moses-mativo <clishtivo@gmail.com>
2022-05-23 11:05:47 +0300

    fav icon
    almost

--- js/main.js
+++ js/main.js
@@ -88,4 +88,13 @@ $(document).ready(function() {
     }
 });
 
-    });
+});
+
+    window.onload = function(){
+        let player = document.getElementById("player"),
+            play = document.getElementById("play");
+        
+        play.addEventListener("click",function(){
+          player.play();
+        });
+      }
